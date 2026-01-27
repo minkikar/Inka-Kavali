23d69cda6dbcd18fcdc557ff89698d70108651e8
minkikar <minkikaishrao@gmail.com>
2026-01-27 14:17:56 +0530

    version 1.0 added address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,8 @@ export default function Footer() {
       <div className="footer-container">
         <p className="footer-tagline">Made with care, just like at home.</p>
         <div className="footer-brand">Inka Kavali</div>
+        <p>By Shree Sai Gayatri Home Foods</p>
+        <p>Dharmabad Maharashtra</p>
         <div className="footer-bottom">
           <p className="copyright">
             © {new Date().getFullYear()} Inka Kavali. All rights reserved.
